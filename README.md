de4dace7d8aef9266c25a8f9018a15f32e5eb3a9
Rui Carmo <rui.carmo@gmail.com>
2014-03-31 13:22:07 +0100

    Added boilerplate

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ rss2imap-gae
 ============
 
 A reboot of rss2imap to run on Google App Engine
+
+    pip install -r requirements.txt -t ./lib
---
+++ main.py
@@ -0,0 +1,3 @@
+# Stub to load Hy
+from hy.importer import import_file_to_module
+__hymain__ = import_file_to_module('__hymain__', 'main.hy')
